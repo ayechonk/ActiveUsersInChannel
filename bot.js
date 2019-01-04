f220494b801ebeed1d083679266571b0faca2cf9
Chris Honkonen <chrishonkonen@gmail.com>
2019-01-04 16:29:48 -0500

    work on bot class and roles

--- bot.js
+++ bot.js
@@ -1,16 +1,64 @@
-module.exports = function(){
-	this.test = "test";
+module.exports = function(client) {
+	this.client = client;
 	this.roster = [];
+
 	this.getMemberFromRoster = memberId => {
 		let currentMember = null;
-		let numOfAttended = roster.length;
+		let numOfAttended = this.roster.length;
 		for (let i = 0; i < numOfAttended; i++) {
-			let currentRosterMember = roster[i];
-			if (currentRosterMember.id === memberId) {
-				currentMember = currentRosterMember;
+			let loopMember = this.roster[i];
+			if (loopMember.id === memberId) {
+				currentMember = loopMember;
 				break;
 			}
 		}
-		return currentMember
+		return currentMember;
+	};
+
+	this.consumeMessage = (msg) => {
+		if (msg.content === 'clear') {
+			this.clearRoster();
+			msg.reply('Users have been cleared');
+		}
+		else if (msg.content === 'roster') {
+			msg.reply(JSON.stringify(this.roster));
+		}
+		else if (msg.content === 'roles') {
+			// msg.reply(JSON.stringify(this.client.guilds));
+			msg.reply(JSON.stringify(this.client.guilds.first().roles.find("name", "buttsniffer").members));
+			// let guild = this.client.guilds.first();
+			// let mapRoles = guild.roles;
+			// for (let [key, role] of mapRoles) {
+			// 	console.log(role);
+			// }
+		}
+		else {
+			let memberId = msg.author.id;
+			let member = this.getMemberFromRoster(memberId);
+			if (member === null) {
+				this.pushToRoster(memberId)
+			}
+			else {
+				this.updateToRoster(member)
+			}
+			console.log(JSON.stringify(this.roster))
+		}
+	};
+
+	this.clearRoster = () => {
+		this.roster = [];
+	};
+
+	this.pushToRoster = (newUserId) => {
+		this.roster.push({
+			"id": newUserId,
+			"lastAttendance": Date.now()
+		})
+	};
+
+	this.updateToRoster = member => {
+		if (member !== null) {
+			member.lastAttendance = Date.now()
+		}
 	};
 };
--- server.js
+++ server.js
@@ -1,71 +1,28 @@
 require('dotenv').config();
-
 let Bot = require('./bot.js');
 let Discord = require('discord.js');
+
+let bot;
 let client = new Discord.Client();
-let roster = [];
-
-let getMemberFromRoster = memberId => {
-	let currentMember = null;
-	let numOfAttended = roster.length;
-	for (let i = 0; i < numOfAttended; i++) {
-		let currentRosterMember = roster[i];
-		if (currentRosterMember.id === memberId) {
-			currentMember = currentRosterMember;
-			break;
-		}
-	}
-	return currentMember
-};
-
-let updateToRoster = member => {
-	if (member !== null) {
-		member.lastAttendance = Date.now()
-	}
-};
-
-let pushToRoster = memberId => {
-	roster.push({
-		"id": memberId,
-		"lastAttendance": Date.now()
-	})
-};
 
 client.on('voiceStateUpdate', (oldMember, newMember) => {
 	if (newMember.voiceChannelID !== null) {
 		let member = getMemberFromRoster(newMember.user.id);
 		if (member === null) {
-			pushToRoster(newMember.user.id)
+			bot.pushToRoster(newMember.user.id)
 		} else {
-			updateToRoster(member)
+			bot.updateToRoster(member)
 		}
 	}
 });
 
 client.on('message', msg => {
 	if (msg.author.id !== client.user.id) {
-		if (msg.content === 'clear') {
-			roster = [];
-			msg.reply('Users have been cleared')
-		} else if (msg.content === 'roster') {
-			let usersString = JSON.stringify(roster);
-			msg.reply(usersString)
-		} else {
-			let member = getMemberFromRoster(client.user.id);
-			if (member === null) {
-				pushToRoster(msg.author.id)
-			} else {
-				updateToRoster(member)
-			}
-			console.log(JSON.stringify(roster))
-		}
+		bot.consumeMessage(msg);
 	}
 });
 
-client
-.login(process.env.BOT_TOKEN)
-.then(
-	function () {
-		console.log(new Bot());
-		console.log('connected successfully');
-	});
+client.login(process.env.BOT_TOKEN).then(() => {
+	bot = new Bot(client);
+	console.log('connected successfully');
+});
